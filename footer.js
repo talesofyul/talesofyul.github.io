c8120b2cf891f6c4b355cc517048b1252b78b1ab
Ignacio Rodriguez <ignacionr@gmail.com>
2024-02-13 18:04:23 -0300

    Add footer to index.html and articles

---
+++ footer.js
@@ -0,0 +1,29 @@
+function loadJSON(callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.overrideMimeType("application/json");
+  xhr.open("GET", "index.json", true);
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      callback(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.send(null);
+}
+
+// Function to populate footer element
+function populateFooter() {
+  loadJSON(function (data) {
+    var footer = document.getElementsByTagName("footer");
+    var content = "<h2>Recent Investigation Results</h2>";
+
+    // Assuming the JSON file contains an array of strings
+    data.forEach(function (item) {
+      content += "<p>" + item + "</p>";
+    });
+
+    footer.innerHTML = content;
+  });
+}
+
+// Call the function to populate the footer
+populateFooter();
