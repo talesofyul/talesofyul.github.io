6e3b15e7efa8dd147b1ea60e40a4faa399dcee07
Ignacio Rodriguez <ignacionr@gmail.com>
2024-02-25 11:46:26 +0100

    added note on footer

--- footer.js
+++ footer.js
@@ -1,25 +1,32 @@
-function loadJSON(callback) {
+function loadText(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.overrideMimeType("application/json");
-  xhr.open("GET", "index.json", true);
+  xhr.open("GET", url, true);
   xhr.onreadystatechange = function () {
     if (xhr.readyState === 4 && xhr.status === 200) {
-      callback(JSON.parse(xhr.responseText));
+      callback(xhr.responseText);
     }
   };
   xhr.send(null);
 }
 
+function loadJSON(url, callback) {
+  loadText(url, function (text) {
+    callback(JSON.parse(text));
+  })
+}
+
 // Function to populate footer element
 function populateFooter() {
-  loadJSON(function (data) {
-    var footer = document.getElementsByTagName("footer")[0];
-    var content = "";
-    data.forEach(function (item) {
-      content += "<div class='card'>" + item + "</div>";
-    });
+  loadJSON("index.json", function (data) {
+    loadText("note.html.partial", function (content) {
+      var footer = document.getElementsByTagName("footer")[0];
+      data.forEach(function (item) {
+        content += "<div class='card'>" + item + "</div>";
+      });
 
-    footer.innerHTML = content;
+      footer.innerHTML = content;
+    });
   });
 }
 
